d6a70b788f4d233c272de5a7decdec2b1151bbac
pirent <pirent420@gmail.com>
2018-05-09 06:22:21 +0700

    Think Python: exercise 1303 and 1304

--- thinkpython/exercise1302to04.py
+++ thinkpython/exercise1302to04.py
@@ -1,14 +1,23 @@
 # ======================================================================
+# Exercise 13.02
 # Modify your program from the previous exercise to read the book you downloaded, skip over the
 # header information at the beginning of the file, and process the rest of the words as before.
 # 
 # Then modify the program to count the total number of words in the book, and the number of times
 # each word is used.
 #
-# Print the number of different words used in the book. Compare different books by different authors,
-#written in different eras. Which author uses the most extensive vocabulary?
+# Print the number of different words used in the book. 
+#
+# Exercise 13.03
+# Modify the program from the previous exercise to print the 20 most frequently used words
+#
+# Exercise 13.04
+# Modify the previous program to read a word list (see Section 9.1) and then print all
+# the words in the book that are not in the word list. How many of them are typos? How many of
+# them are common words that should be in the word list, and how many of them are really obscure?
 # ======================================================================
 from exercise1301 import *
+from list_exercise import make_list_with_append
 
 def find_line_number_of_start_line(filename):
   start_line_pattern = "*** START"
@@ -38,6 +47,18 @@ def count_total_words(histogram):
     total += count
   return total
 
+def find_most_frequently_used_words(histogram, number_of_words=10):
+  tuple_list = []
+  for word, count in histogram.items():
+    t = (count, word)
+    tuple_list.append(t)
+  tuple_list.sort(reverse=True)
+  
+  res = []
+  for i in range(number_of_words):
+    res.append(tuple_list[i][1])
+  return res
+
 def process(filename):
   f = open(filename)
 
@@ -45,12 +66,23 @@ def process(filename):
   lines = f.readlines()[start_line_number:]
   histogram = build_histogram(lines)
 
+  f.close 
+
   print("The total number of words in book:", count_total_words(histogram))
   print("The number of different words in book", len(histogram.keys()))
   print("Number of times each word is used")
-  print(histogram)
+  for item in histogram.items():
+     print(item)
+  print("================================")
+  print("Most 20 frequenly used words")
+  print(find_most_frequently_used_words(histogram, 20))
+  
+  word_list = set(make_list_with_append())
+  words_from_book = set(histogram.keys())
+  print("Words from book that are not in word list:", words_from_book - word_list)
 
-  f.close 
+
+# ===========================
 
 if __name__ == "__main__":
   process("short_one.txt")
