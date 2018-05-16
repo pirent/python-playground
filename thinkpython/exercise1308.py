5cdac2a9e6a8046dad63a30b98c66a7fb73cc858
pirent <pirent420@gmail.com>
2018-05-17 05:16:07 +0700

    Chapter 13: Markov analysis v1.0

---
+++ thinkpython/exercise1308.py
@@ -0,0 +1,75 @@
+import string
+import random
+
+to_be_replaced_letter_list = string.whitespace + string.punctuation
+
+
+def build_markov_analysis_from_file(filename, prefix_length=2):
+  res = {}
+  fin = open(filename)
+  end_of_header_pattern = "*END*"
+  
+  for line in fin:
+    if end_of_header_pattern in line:
+      break
+  
+  for line in fin:
+    #if line.startswith("#"):
+    #  continue
+    process_line(line, res, prefix_length)
+
+  return res
+
+def process_line(line, mapping, prefix_length=2):
+  raw_words = line.split()
+  processed_words = []
+  for word in raw_words:
+    word = word.strip(to_be_replaced_letter_list)
+    word = word.lower()
+    processed_words.append(word)
+  
+  #print(">> DEBUG: line = {}".format(line))
+  #print(">> DEBUG: processed_words = {}".format(processed_words))
+  
+  for i in range(len(processed_words) - prefix_length):
+    prefix = " ".join(processed_words[i:i + prefix_length])
+    suffix = processed_words[i + prefix_length]
+    suffixes = mapping.setdefault(prefix, [])
+    suffixes.append(suffix)
+    
+    #print(">> DEBUG: prefix = {}, suffix = {}, suffixes after adding = {}".format(prefix, suffix, suffixes))
+
+def build_randomly_text(mapping, txt_length=10):
+  res = []
+  prefixes = list(mapping.keys())
+  random_prefix = random.choice(prefixes)
+  res.append(random_prefix)
+
+  suffixes = mapping[random_prefix]
+  random_suffix = random.choice(suffixes)
+  res.append(random_suffix)
+
+  print(">> DEBUG: first prefix = {}, first suffix = {}".format(random_prefix, random_suffix))
+
+  previous_prefix = random_prefix
+  previous_suffix = random_suffix
+  for i in range(txt_length):
+    # build next prefix
+    prefix = previous_prefix.split(' ', 1)[1] + " " + previous_suffix
+    suffixes = mapping.get(prefix)
+    if not suffixes:
+      break
+    suffix = random.choice(suffixes)
+    res.append(suffix)
+
+    previous_prefix = prefix
+    previous_suffix = suffix
+
+  return " ".join(res)
+
+if __name__ == "__main__":
+  filename = "emma.txt"
+  mapping = build_markov_analysis_from_file(filename, 3)
+  #print(mapping)
+  random_txt = build_randomly_text(mapping)
+  print("Random txt:", random_txt)
