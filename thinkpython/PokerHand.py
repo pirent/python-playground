a4cf9554c7c4037889a5689c7760dacec517d6f0
pirent <pirent420@gmail.com>
2018-06-03 06:16:10 +0700

    Think Python: chapter 18
    - enhances PokerHand, add classify() method, skip probability calculation function

--- thinkpython/PokerHand.py
+++ thinkpython/PokerHand.py
@@ -13,104 +13,105 @@ from __future__ import print_function, division
 
 from chapter18 import Card, Hand, Deck
 
+class Hist(dict):
+  def __init__(self, seq=[]):
+    for x in seq:
+      self.count(x)
+
+  def count(self, x, f=1):
+    self[x] = self.get(x, 0) + f
+    if self[x] == 0:
+      del self[x]
+
 class PokerHand(Hand):
   """Represents a poker hand."""
 
-  def suit_hist(self, collection):
-    """Builds a histogram of the suits that appear in the hand.
+  all_labels = ["straight_flush", "four_of_a_kind", "full_house", "flush", "straight", "three_of_a_kind", "two_pairs", "one_pair"]
+  
+  def make_histogram(self):
+    """compute the histogram for suit and rank"""
+    self.suits = Hist()
+    self.ranks = Hist()
 
-    Stores the result in attribute suits.
-    """
-    self.suits = {}
-    for card in collection:
-      self.suits[card.suit] = self.suits.get(card.suit, 0) + 1
+    for card in self.cards:
+      self.suits.count(card.suit)
+      self.ranks.count(card.rank)
 
-  def has_flush(self, collection=None):
-    """Returns True if the hand has a flush, False otherwise.
-      
-    Note that this works correctly for hands with more than 5 cards.
+    self.sets = list(self.ranks.values())
+    self.sets.sort(reverse=True)
+
+  def check_set(self, *t):
+    """Check whether self.sets contains sets that are as big as the requirement ti
     """
-    if not collection:
-      collection = self.cards
-    self.suit_hist(collection)
+    for need, have in zip(t, self.sets):
+      if need > have:
+        return False
+    return True
+ 
+  def has_one_pair(self):
+    return self.check_set(2)
+
+  def has_two_pairs(self):
+    return self.check_set(2, 2)
+
+  def has_three_of_a_kind(self):
+    return self.check_set(3)
+
+  def has_four_of_a_kind(self):
+    return self.check_set(4)
+
+  def has_full_house(self):
+    return self.check_set(3, 2)
+
+  def has_flush(self):
     for val in self.suits.values():
       if val >= 5:
         return True
     return False
-
-  def rank_hist(self):
-    self.ranks = {}
-    for card in self.cards:
-      self.ranks[card.rank] = self.ranks.get(card.rank, 0) + 1
-
-  def count_same_rank(self, no_cards_in_same_rank=2):
-    """Return the number of pairs/three of a kind in hands"""
-    counter = 0
-    self.rank_hist()
-    #print(">> DEBUG: histogram of rank is", self.ranks)
-    for val in self.ranks.values():
-      if val == no_cards_in_same_rank:
-        counter+=1
-    return counter
-
-  def has_one_pair(self):
-    return self.count_same_rank() == 1
-
-  def has_two_pairs(self):
-    return self.count_same_rank() == 2
-
-  def has_three_of_a_kind(self):
-    return self.count_same_rank(3) == 1
-
-  def has_four_of_a_kind(self):
-    return self.count_same_rank(4) == 1
-
-  def collect_sequence(self):
-    self.sort(False)
-    res = []
-    for i in range(len(self.cards) - 1):
-      current_card = self.cards[i]
-      next_card = self.cards[i + 1]
-      diff = next_card.rank - current_card.rank
-      if diff == 1 or diff == 0:
-        res.append(self.cards[i])
-        if i == (len(self.cards) - 2):
-          res.append(self.cards[i + 1])
-        self.print_card_list(res)
-    return res
-
-  def print_card_list(self, collection):
-    print("DEBUG, card list is now:", end = " ")
-    for card in collection:
-      print(str(card), end=', ')
-    print()
-
-  def has_straight(self):
-    tmp = self.collect_sequence()
-    print(">> DEBUG - before set")
-    self.print_card_list(tmp)
-    tmp = set(tmp)
-    print(">> DEBUG - after set:")
-    self.print_card_list(tmp)
-    return len(tmp) >= 5
-      
-  def has_full_house(self):
-    return self.has_three_of_a_kind and self.has_one_pair()
     
-  def has_straight_flush(self):
-    tmp = self.collect_sequence()
-    if not self.has_straight():
-      return False
-   
-    is_flush = False
-    tmp_suit = None
-    for card in tmp:
-      if not tmp_suit:
-        tmp_suit = card.suit
+  def has_straight(self):
+    ranks = self.ranks.copy()
+    ranks[14] = self.ranks.get(1, 0)
+
+    return self.in_a_row(ranks, 5)
+
+  def print_cards_seq(self, seq):
+    res = []
+    for card in seq:
+      res.add(str(card))
+    print(", ".join(res))
+
+  def in_a_row(self, ranks, n=5):
+    count = 0
+    for i in range(1, 15):
+      if ranks.get(i, 0):
+        count += 1
+        if count == n:
+          return True
       else:
-        if tmp_suit != card.suit:
-          return False
-    return True
+        count = 0
+    return False
+
+  def has_straight_flush(self):
+    sub_hands = {}
+    for c in self.cards:
+      sub_hands.setdefault(c.suit, PokerHand()).add_card(c)
+
+    for hand in sub_hands.values():
+      if len(hand.cards) < 5:
+        continue
+      hand.make_histogram()
+      if hand.has_straight():
+        return True
+    return False
+
+  def classify(self):
+    self.make_histogram()
+    self.my_labels = set()
+    for label in PokerHand.all_labels:
+      f = getattr(self, "has_" + label)
+      if f():
+        self.my_labels.add(label)
 
 if __name__ == '__main__':
   ## make a deck
@@ -147,11 +148,13 @@ if __name__ == '__main__':
   hand1.add_card(three_of_hearts)
   hand1.add_card(four_of_spades)
 
-  print("Hand One has flush? >>", hand1.has_flush())
-  print("Hand One has straight? >>", hand1.has_straight())
-  print("Hand One has one pair? >>", hand1.has_one_pair())
-  print("Hand One has two pairs? >>", hand1.has_two_pairs())
-  print("Hand One has straight flush? >>", hand1.has_straight_flush())
+  hand1.classify()
+
+  print("Hand One has flush? >>", "flush" in hand1.my_labels)
+  print("Hand One has straight? >>", "straight" in hand1.my_labels)
+  print("Hand One has one pair? >>", "one_pair" in hand1.my_labels)
+  print("Hand One has two pairs? >>", "two_pair" in hand1.my_labels)
+  print("Hand One has straight flush? >>", "straight_flush" in hand1.my_labels)
 
   hand2 = PokerHand()
   hand2.add_card(three_of_clubs)
@@ -159,11 +162,12 @@ if __name__ == '__main__':
   hand2.add_card(three_of_diamonds)
   hand2.add_card(four_of_clubs)
   hand2.add_card(four_of_spades)
-
-  print("Hand Two has three of a kind? >>", hand2.has_three_of_a_kind())
-  print("Hand Two has full house? >>", hand2.has_full_house()) 
-  
   hand2.add_card(three_of_spades)
+
+  hand2.classify()
+  print("Hand Two has three of a kind? >>", "three_of_a_kind" in hand2.my_labels)
+  print("Hand Two has full house? >>", "full_house" in hand2.my_labels) 
   
-  print("Hand Two has four of kinds? >>", hand2.has_four_of_a_kind())
+  
+  print("Hand Two has four of a kind? >>", "four_of_a_kind" in hand2.my_labels)
   
